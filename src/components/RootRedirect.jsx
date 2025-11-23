08f53bff1c09ef8a566f6860c7b85dbcf1c2bebf
Jayesh Padwal <jayeshpadwal09@gmail.com>
2025-11-23 20:51:36 +0530

    refactor RootRedirect to use selectUser from authSlice for user state management

--- src/components/RootRedirect.jsx
+++ src/components/RootRedirect.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
-import Login from '@/pages/Login'
-import Home from '@/pages/Home'
+import { selectUser } from '@/features/authSlice'
 
 export default function RootRedirect() {
-  const user = useSelector((s) => s.auth.userData)
+  const user = useSelector(selectUser)
 
+  // Keep URL consistent: send authenticated users to /Home, otherwise to /login
   if (user) return <Navigate to="/Home" replace />
-  return <Login />
+  return <Navigate to="/login" replace />
 }
